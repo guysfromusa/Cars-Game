e9525608a56b99ecbfaf001cca6c4f6234f789b6
tomek <disnej2@gmail.com>
2018-02-24 23:45:26 +0100

    app generated using SpringBoot initialized

---
+++ src/main/java/com/guysfromusa/carsgame/CarsgameApplication.java
@@ -0,0 +1,12 @@
+package com.guysfromusa.carsgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsgameApplication.class, args);
+	}
+}
