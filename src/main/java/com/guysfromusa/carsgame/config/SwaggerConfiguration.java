72fa07e30dbfcc9aba83c7181530bae7ecde7f42
Dominik <dominik.zurek1102@gmail.com>
2018-02-28 00:47:09 +0100

    into movments-history

--- src/main/java/com/guysfromusa/carsgame/config/SwaggerConfiguration.java
+++ src/main/java/com/guysfromusa/carsgame/config/SwaggerConfiguration.java
@@ -3,7 +3,6 @@ package com.guysfromusa.carsgame.config;
 import com.google.common.collect.ImmutableList;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
