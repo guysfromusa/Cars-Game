d6be941dec1f42c5638d536aa35b7be3c8140bf1
tbradlo <tbradlo@gmail.com>
2018-02-28 21:51:56 +0100

    Starting the game

---
+++ src/main/java/com/guysfromusa/carsgame/services/GameService.java
@@ -0,0 +1,45 @@
+package com.guysfromusa.carsgame.services;
+
+import com.guysfromusa.carsgame.entities.GameEntity;
+import com.guysfromusa.carsgame.entities.MapEntity;
+import com.guysfromusa.carsgame.repositories.GameRepository;
+import com.guysfromusa.carsgame.repositories.MapRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import java.util.Optional;
+
+import static org.apache.commons.lang3.Validate.notNull;
+
+/**
+ * Created by Tomasz Bradlo, 28.02.18
+ */
+@Service
+public class GameService {
+
+    private final GameRepository gameRepository;
+
+    private final MapRepository mapRepository;
+
+    @Inject
+    public GameService(GameRepository gameRepository, MapRepository mapRepository) {
+        this.gameRepository = notNull(gameRepository);
+        this.mapRepository = notNull(mapRepository);
+    }
+
+    @Transactional
+    public GameEntity startNewGame(String gameName, String mapName) {
+        Optional<MapEntity> mapOptional = mapRepository.findByNameAndDeleted(mapName, false);
+
+        //TODO negative response if game already exist
+
+        MapEntity map = mapOptional
+                .orElseThrow(() -> new IllegalArgumentException("Map '" + mapName + "' not found"));
+
+        GameEntity game = new GameEntity();
+        game.setName(gameName);
+        game.setMap(map);
+        return gameRepository.save(game);
+    }
+}
---
+++ src/test/java/com/guysfromusa/carsgame/services/GameServiceTest.java
@@ -0,0 +1,58 @@
+package com.guysfromusa.carsgame.services;
+
+import com.guysfromusa.carsgame.entities.GameEntity;
+import com.guysfromusa.carsgame.entities.MapEntity;
+import com.guysfromusa.carsgame.repositories.GameRepository;
+import com.guysfromusa.carsgame.repositories.MapRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Tomasz Bradlo, 28.02.18
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class GameServiceTest {
+
+    @InjectMocks
+    private GameService gameService;
+
+    @Mock
+    private GameRepository gameRepositoryMock;
+
+    @Mock
+    private MapRepository mapRepositoryMock;
+
+    @Captor
+    private ArgumentCaptor<GameEntity> gameEntityCaptor;
+
+    @Test
+    public void shouldStartNewGame() {
+        //given
+        MapEntity mapEntity = new MapEntity();
+        mapEntity.setName("map1");
+        when(mapRepositoryMock.findByNameAndDeleted("map1", false))
+                .thenReturn(Optional.of(mapEntity));
+
+        //when
+        gameService.startNewGame("game1", "map1");
+
+        //then
+        verify(gameRepositoryMock).save(gameEntityCaptor.capture());
+        GameEntity gotGame = gameEntityCaptor.getValue();
+
+        assertThat(gotGame.getMap().getName()).isEqualTo("map1");
+        assertThat(gotGame.getName()).isEqualTo("game1");
+    }
+
+}
