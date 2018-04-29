ad9c5e9595352eea9a8ebedbdbfb6ee935fc8668
Sebastian Mikucki <sebastian.mikucki@gmail.com>
2018-04-29 09:01:47 +0200

    MapConverter refactoring

--- src/main/java/com/guysfromusa/carsgame/v1/converters/MapConverter.java
+++ src/main/java/com/guysfromusa/carsgame/v1/converters/MapConverter.java
@@ -2,17 +2,18 @@ package com.guysfromusa.carsgame.v1.converters;
 
 import com.guysfromusa.carsgame.entities.MapEntity;
 import com.guysfromusa.carsgame.v1.model.Map;
-import lombok.NoArgsConstructor;
-
-import static lombok.AccessLevel.PRIVATE;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Robert Mycek, 2018-02-26
  */
-@NoArgsConstructor(access = PRIVATE)
-public class MapConverter {
+@Component
+public class MapConverter implements Converter<MapEntity, Map> {
 
-    public static Map fromEntity(MapEntity mapEntity) {
+    @Override
+    public Map convert(MapEntity mapEntity) {
         return new Map(mapEntity.getName(), mapEntity.getContent());
     }
+
 }
