1acfa164a4273d70e3bc65573cf556400a555e3f
konrad <konradru2@gmail.com>
2018-04-30 01:01:09 +0200

    Car addition to game - remove validation exception class

--- src/main/java/com/guysfromusa/carsgame/exceptions/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.guysfromusa.carsgame.exceptions;
-
-public class ValidationException extends RuntimeException{
-
-    public ValidationException(String message){
-        super(message);
-    }
-}
