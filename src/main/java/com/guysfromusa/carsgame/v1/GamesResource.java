885872c5c42024dcfc43ce1a512e238738c06962
Sebastian Mikucki <sebastian.mikucki@gmail.com>
2018-04-30 12:51:12 +0200

    Expose API to check status game

--- src/main/java/com/guysfromusa/carsgame/v1/GamesResource.java
+++ src/main/java/com/guysfromusa/carsgame/v1/GamesResource.java
@@ -3,17 +3,16 @@ package com.guysfromusa.carsgame.v1;
 import com.google.common.collect.Maps;
 import com.guysfromusa.carsgame.entities.CarEntity;
 import com.guysfromusa.carsgame.entities.GameEntity;
+import com.guysfromusa.carsgame.entities.enums.GameStatus;
 import com.guysfromusa.carsgame.services.CarService;
 import com.guysfromusa.carsgame.services.GameService;
 import com.guysfromusa.carsgame.utils.StreamUtils;
 import com.guysfromusa.carsgame.v1.model.Car;
 import com.guysfromusa.carsgame.v1.model.Game;
+import com.guysfromusa.carsgame.v1.model.GameStatusDto;
 import com.guysfromusa.carsgame.v1.model.Movement;
 import com.guysfromusa.carsgame.v1.movement.MovementStrategy;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.web.bind.annotation.*;
 
@@ -77,6 +76,19 @@ public class GamesResource {
     public Game startNewGame(@PathVariable("gameName") String gameName, @RequestBody String mapName){
         GameEntity gameEntity = gameService.startNewGame(gameName, mapName);
         return conversionService.convert(gameEntity, Game.class);
+    }
+
+    @GetMapping(path = "{gameName}")
+    @ApiOperation(value = "Check status for the given game")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Status of the game"),
+            @ApiResponse(code = 404, message = "Game not found")
+
+    })
+    public GameStatusDto checkStatus(@ApiParam(name = "gameName", value = "name of the game", required = true)
+                                                @PathVariable("gameName") String gameName){
+        GameStatus status = gameService.getStatus(gameName);
+        return conversionService.convert(status, GameStatusDto.class);
     }
 
 }
