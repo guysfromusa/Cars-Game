771b4d3c198b738ac87295af6ec0e40d3de1bb18
Dominik <dominik.zurek1102@gmail.com>
2018-04-30 22:06:50 +0200

    into movments-history-add resource-add resource test without parameter

--- src/test/java/com/guysfromusa/carsgame/v1/MovementsHistoryResourceTest.java
+++ src/test/java/com/guysfromusa/carsgame/v1/MovementsHistoryResourceTest.java
@@ -26,7 +26,7 @@ public class MovementsHistoryResourceTest {
     private TestRestTemplate template;
 
     @Test
-    @Sql("/sql/historyMovement_Insert.sql")
+    @Sql(value = {"/sql/clean.sql","/sql/historyMovement_Insert.sql"})
     public void shouldReturnOne() {
         //when
         ResponseEntity<MovementHistory[]> movements = template.getForEntity("/v1/movements-history?gameIds=&carNames=FIAT&limitOfRecentStep=", MovementHistory[].class);
@@ -38,7 +38,7 @@ public class MovementsHistoryResourceTest {
   }
 
     @Test
-    @Sql("/sql/historyMovement_Insert.sql")
+    @Sql(value = {"/sql/clean.sql","/sql/historyMovement_Insert.sql"})
     public void shouldReturnTwo() {
         //when
         ResponseEntity<MovementHistory[]> movements = template.getForEntity("/v1/movements-history?gameIds=game2,game3&carNames=FIAT,MERCEDES&limitOfRecentStep=", MovementHistory[].class);
@@ -50,7 +50,7 @@ public class MovementsHistoryResourceTest {
     }
 
     @Test
-    @Sql("/sql/historyMovement_Insert.sql")
+    @Sql(value = {"/sql/clean.sql","/sql/historyMovement_Insert.sql"})
     public void shouldReturnTree() {
         //when
         ResponseEntity<MovementHistory[]> movements = template.getForEntity("/v1/movements-history?gameIds=&carNames=&limitOfRecentStep=", MovementHistory[].class);
