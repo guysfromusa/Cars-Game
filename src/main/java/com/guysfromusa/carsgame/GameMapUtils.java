282da24ac033fb244127e3dd78c6946a485b95c1
rmycek <robert.mycek@gmail.com>
2018-05-02 16:12:35 +0200

    maps: rename point indexes from (i,j) to (x,y)

--- src/main/java/com/guysfromusa/carsgame/GameMapUtils.java
+++ src/main/java/com/guysfromusa/carsgame/GameMapUtils.java
@@ -37,11 +37,11 @@ public class GameMapUtils {
                 .map(row -> Stream.of(row).map(v -> v != 0).toArray(Boolean[]::new))
                 .toArray(Boolean[][]::new);
 
-        // queue of point - tuple of indexes (i,j) to visit
+        // queue of point to visit - tuple of indexes (x,y)
         Queue<Tuple2<Integer, Integer>> queue = new LinkedList<>();
 
-        // find root: first valid point on map
-        Tuple2<Integer, Integer> root = findRoot(map);
+        // find root: any valid point on map
+        Tuple2<Integer, Integer> root = findValidPoint(map);
         if (root == null) {
             return false;
         }
@@ -49,16 +49,16 @@ public class GameMapUtils {
 
         while (!queue.isEmpty()) {
             Tuple2<Integer, Integer> indexes = queue.poll();
-            int i = indexes._1;
-            int j = indexes._2;
+            int x = indexes._1;
+            int y = indexes._2;
 
-            visited[i][j] = true;
+            visited[x][y] = true;
 
-            // add not visited neighbor
-            addNeighbor(i, j + 1, map, visited, queue);
-            addNeighbor(i, j - 1, map, visited, queue);
-            addNeighbor(i + 1, j, map, visited, queue);
-            addNeighbor(i - 1, j, map, visited, queue);
+            // add not visited neighbors
+            addNeighbor(x, y + 1, map, visited, queue);
+            addNeighbor(x, y - 1, map, visited, queue);
+            addNeighbor(x + 1, y, map, visited, queue);
+            addNeighbor(x - 1, y, map, visited, queue);
         }
 
         // all points should be visited
@@ -67,24 +67,30 @@ public class GameMapUtils {
                 .allMatch(v -> v);
     }
 
-    private static Tuple2<Integer, Integer> findRoot(Integer[][] map) {
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                if (map[i][j] == 0) {
-                    return Tuple(i, j);
+    private static Tuple2<Integer, Integer> findValidPoint(Integer[][] map) {
+        for (int x = 0; x < map.length; x++) {
+            for (int y = 0; y < map[x].length; y++) {
+                if (map[x][y] == 0) {
+                    return Tuple(x, y);
                 }
             }
         }
         return null;
     }
 
-    private static void addNeighbor(int i, int j, Integer[][] map, Boolean[][] visited, Queue<Tuple2<Integer, Integer>> queue) {
-        if (isOnMap(i, j, map) && !visited[i][j]) {
-            queue.offer(Tuple(i, j));
+    private static void addNeighbor(int x, int y, Integer[][] map, Boolean[][] visited, Queue<Tuple2<Integer, Integer>> queue) {
+        if (isOnMap(x, y, map) && !visited[x][y]) {
+            queue.offer(Tuple(x, y));
         }
     }
 
-    private static boolean isOnMap(int i, int j, Integer[][] map) {
-        return i >= 0 && i < map.length && j >= 0 && j < map[i].length;
+    private static boolean isOnMap(int x, int y, Integer[][] map) {
+        return isValidIndex(x, map) && isValidIndex(y, map[x]);
     }
+
+    private static <T> boolean isValidIndex(int index, T[] array) {
+        return index >= 0 && index < array.length;
+    }
+
+
 }
