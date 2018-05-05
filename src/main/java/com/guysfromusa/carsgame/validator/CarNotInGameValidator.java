5512c567c93837932c36256ea80d49718960c660
Sebastian Mikucki <sebastian.mikucki@gmail.com>
2018-05-05 21:24:22 +0200

    Validator small update

--- src/main/java/com/guysfromusa/carsgame/validator/CarNotInGameValidator.java
+++ src/main/java/com/guysfromusa/carsgame/validator/CarNotInGameValidator.java
@@ -2,19 +2,18 @@ package com.guysfromusa.carsgame.validator;
 
 import com.guysfromusa.carsgame.game_state.ActiveGamesContainer;
 import com.guysfromusa.carsgame.game_state.dtos.GameState;
-import com.guysfromusa.carsgame.v1.model.Car;
 import com.guysfromusa.carsgame.validator.subject.CarGameAdditionValidationSubject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
-import java.util.List;
 
-import static com.guysfromusa.carsgame.utils.StreamUtils.convert;
 import static org.apache.commons.lang3.Validate.notNull;
 
 /**
  * Created by Sebastian Mikucki, 04.05.18
  */
+@Slf4j
 @Component
 public class CarNotInGameValidator implements BusinessValidator<CarGameAdditionValidationSubject> {
 
@@ -30,9 +29,9 @@ public class CarNotInGameValidator implements BusinessValidator<CarGameAdditionV
     @Override
     public void validate(CarGameAdditionValidationSubject subject) {
         GameState gameState = activeGamesContainer.getGameState(subject.getGameEntity().getName());
-        List<String> gamesCarNames = convert(gameState.getAllCars(), Car::getName);
-        String carName = subject.getCarEntity().getName();
-        if(gamesCarNames.contains(carName)){
+        String carToAddName = subject.getCarEntity().getName();
+        if(gameState.getAllCars().stream().anyMatch(car -> car.getName().equals(carToAddName))){
+            log.debug("Car: {} already in game", subject.getCarEntity());
             throw new IllegalArgumentException(CAR_EXISTS_IN_GAME_MESSAGE);
         }
     }
