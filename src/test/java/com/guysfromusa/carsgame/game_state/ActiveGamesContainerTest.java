43f9d5df27f5c58ebdfb45b978764c7dd0ad816b
Dominik <dominik.zurek1102@gmail.com>
2018-05-05 12:05:57 +0200

    into undo-posibility-first part

--- src/test/java/com/guysfromusa/carsgame/game_state/ActiveGamesContainerTest.java
+++ src/test/java/com/guysfromusa/carsgame/game_state/ActiveGamesContainerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.Collection;
 
+import static com.guysfromusa.carsgame.entities.CarEntityBuilder.aCarEntity;
 import static com.guysfromusa.carsgame.game_state.dtos.Movement.Operation.FORWARD;
 import static com.guysfromusa.carsgame.game_state.dtos.Movement.Operation.LEFT;
 import static com.guysfromusa.carsgame.game_state.dtos.Movement.Operation.RIGHT;
@@ -32,20 +33,20 @@ public class ActiveGamesContainerTest {
         activeGamesContainer.addNewGame("game1");
 
         //when
-        activeGamesContainer.addNewCar("game1", "bmw");
+        activeGamesContainer.getGameState("game1").addNewCar(aCarEntity().name("bmw").build());
 
         //then
-        assertThat(activeGamesContainer.getCarsMovementHistory( "game1","bmw")).isEmpty();
+        assertThat(activeGamesContainer.getGameState("game1").getMovementHistory("bmw")).isEmpty();
     }
 
     @Test
     public void shouldAddNewCarWithMovement(){
         //given
         activeGamesContainer.addNewGame("game1");
-        activeGamesContainer.addNewCar("game1", "bmw");
+        activeGamesContainer.getGameState("game1").addNewCar(aCarEntity().name("bmw").build());
 
         //when
-        activeGamesContainer.addExecutedMove("game1","bmw", FORWARD);
+        activeGamesContainer.getGameState("game1").addMovementHistory("bmw", FORWARD);
 
         //then
         assertThat(activeGamesContainer.getGameState("game1")
@@ -57,12 +58,13 @@ public class ActiveGamesContainerTest {
     public void shouldReturnTreeLastMove() {
         //given
         activeGamesContainer.addNewGame("game1");
-        activeGamesContainer.addNewCar("game1", "bmw");
-        activeGamesContainer.addExecutedMove("game1", "bmw", FORWARD);
-        activeGamesContainer.addExecutedMove("game1", "bmw", LEFT);
-        activeGamesContainer.addExecutedMove("game1", "bmw", FORWARD);
-        activeGamesContainer.addExecutedMove("game1", "bmw", RIGHT);
-        activeGamesContainer.addExecutedMove("game1", "bmw", RIGHT);
+        activeGamesContainer.getGameState("game1").addNewCar(aCarEntity().name("bmw").build());
+        activeGamesContainer.getGameState("game1").addMovementHistory( "bmw", FORWARD);
+        activeGamesContainer.getGameState("game1").addMovementHistory( "bmw", LEFT);
+        activeGamesContainer.getGameState("game1").addMovementHistory( "bmw", FORWARD);
+        activeGamesContainer.getGameState("game1").addMovementHistory( "bmw", RIGHT);
+        activeGamesContainer.getGameState("game1").addMovementHistory( "bmw", RIGHT);
+
         //when
         Collection<Movement> result = activeGamesContainer.getNCarsMovementHistory("game1", "bmw", 3);
 
@@ -76,7 +78,7 @@ public class ActiveGamesContainerTest {
     public void shouldReturnEmpty(){
         //given
         activeGamesContainer.addNewGame("game1");
-        activeGamesContainer.addNewCar("game1", "bmw");
+        activeGamesContainer.getGameState("game1").addNewCar(aCarEntity().name("bmw").build());
 
         //when
         Collection<Movement> result = activeGamesContainer.getNCarsMovementHistory("fake", "bmw", 3);
