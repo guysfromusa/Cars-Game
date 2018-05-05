419c0f93f38d863ad2bef4afa31cf8fe67717955
Sebastian Mikucki <sebastian.mikucki@gmail.com>
2018-05-05 22:22:24 +0200

    Minor update after code review

--- src/main/java/com/guysfromusa/carsgame/validator/CarNotInGameValidator.java
+++ src/main/java/com/guysfromusa/carsgame/validator/CarNotInGameValidator.java
@@ -2,12 +2,14 @@ package com.guysfromusa.carsgame.validator;
 
 import com.guysfromusa.carsgame.game_state.ActiveGamesContainer;
 import com.guysfromusa.carsgame.game_state.dtos.GameState;
+import com.guysfromusa.carsgame.v1.model.Car;
 import com.guysfromusa.carsgame.validator.subject.CarGameAdditionValidationSubject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 
+import static com.guysfromusa.carsgame.utils.StreamUtils.anyMatch;
 import static org.apache.commons.lang3.Validate.notNull;
 
 /**
@@ -30,7 +32,7 @@ public class CarNotInGameValidator implements BusinessValidator<CarGameAdditionV
     public void validate(CarGameAdditionValidationSubject subject) {
         GameState gameState = activeGamesContainer.getGameState(subject.getGameEntity().getName());
         String carToAddName = subject.getCarEntity().getName();
-        if(gameState.getAllCars().stream().anyMatch(car -> car.getName().equals(carToAddName))){
+        if(anyMatch(gameState.getAllCars(), Car::getName, carToAddName::equals)){
             log.debug("Car: {} already in game", subject.getCarEntity());
             throw new IllegalArgumentException(CAR_EXISTS_IN_GAME_MESSAGE);
         }
